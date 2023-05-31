69cc91a7ad6b930c3c3b91f5fc4dea94c9f70334
osugiyuya <mande_e@ezweb.ne.jp>
2023-05-31 19:46:57 +0900

    stylistDelete

--- resources/views/admin/stylist/index.blade.php
+++ resources/views/admin/stylist/index.blade.php
@@ -14,7 +14,7 @@
                     <section class="text-gray-600 body-font">
                         <div class="container px-5 py-24 mx-auto">
 
-                            <x-flash-message status="info" />
+                            <x-flash-message status="session('status')" />
 
                             <div class="flex justify-end mb-4">
                                 <button onclick="location.href='{{ route('admin.stylist.create') }}'" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">新規登録</button>
@@ -27,9 +27,10 @@
                             <table class="table-auto w-full text-left whitespace-no-wrap">
                                 <thead>
                                 <tr>
-                                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100  rounded-tl rounded-bl">名前</th>
+                                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">名前</th>
                                     <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">作成日</th>
-                                    <th class="w-10 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
+                                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
+                                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
                                 </tr>
                                 </thead>
                                 <tbody>
@@ -37,10 +38,16 @@
                                     <tr>
                                         <td class="px-4 py-3">{{ $stylist->name }}</td>
                                         <td class="px-4 py-3">{{ $stylist->created_at }}</td>
-                                        <td class="w-10 text-center">
-                                        {{-- <input name="plan" type="radio"> --}}
-                                        <button onclick="location.href='{{ route('admin.stylist.edit', ['stylist' => $stylist->id]) }}'" class="text-gray bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded">編集する</button>
+                                        <td class="px-4 py-3">
+                                            <button onclick="location.href='{{ route('admin.stylist.edit', ['stylist' => $stylist->id]) }}'" class="text-gray bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded">編集する</button>
                                         </td>
+                                        <form id="delete_{{$stylist->id}}" method="post" action="{{ route('admin.stylist.destroy', ['stylist' => $stylist->id]) }}">
+                                            @csrf
+                                            @method('delete')
+                                            <td class="px-4 py-3">
+                                                <a href="#" data-id="{{$stylist->id}}" onclick="deletePost(this)" class="text-gray bg-rede-400 border-0 py-2 px-8 focus:outline-none hover:bg-red-500 rounded">削除する</a>
+                                            </td>
+                                        </form>
                                     </tr>
                                     @endforeach
                                 </tbody>
@@ -53,4 +60,14 @@
             </div>
         </div>
     </div>
+
+    <script>
+        function deletePost(e) {
+            'use strict';
+            if (confirm('本当に削除してもいいですか？')) {
+                document.getElementById('delete_' + e.dataset.id).submit();
+            }
+        }
+    </script>
+
 </x-app-layout>
