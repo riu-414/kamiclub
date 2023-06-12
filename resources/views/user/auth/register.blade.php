2af0b60e771f0bad37457bb16b870f3c3f6d0d42
osugiyuya <mande_e@ezweb.ne.jp>
2023-06-13 00:43:45 +0900

    welcom

--- resources/views/user/auth/register.blade.php
+++ resources/views/user/auth/register.blade.php
@@ -1,24 +1,29 @@
 <x-guest-layout>
+
+    <div class="flex justify-center">
+        新規登録
+    </div>
+
     <form method="POST" action="{{ route('user.register') }}">
         @csrf
 
         <!-- Name -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
+            <x-input-label for="name" value="名前" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
         <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
+            <x-input-label for="email" value="メールアドレス" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" value="パスワード" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
@@ -30,7 +35,7 @@
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <x-input-label for="password_confirmation" value="パスワードの確認" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
                             type="password"
@@ -41,11 +46,11 @@
 
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('user.login') }}">
-                {{ __('Already registered?') }}
+                登録済みの方はこちら
             </a>
 
             <x-primary-button class="ml-4">
-                {{ __('Register') }}
+                登録する
             </x-primary-button>
         </div>
     </form>
