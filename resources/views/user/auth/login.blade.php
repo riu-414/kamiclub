81ff08f4b30d4e43c2df8664b112edfdee28af13
osugiyuya <mande_e@ezweb.ne.jp>
2023-06-15 00:45:42 +0900

    user_LoginRegisetr

--- resources/views/user/auth/login.blade.php
+++ resources/views/user/auth/login.blade.php
@@ -1,47 +1,56 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+@extends('layouts.layout')
 
-    <form method="POST" action="{{ route('user.login') }}">
-        @csrf
+@section('title')
+    kamiclub
+@endsection
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" value="メールアドレス" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+@section('content')
+    <div class="login-page d-flex flex-column justify-content-center align-items-center ">
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" value="パスワード" />
+        <h2 class="login-h2">会員ログイン</h2>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+        <form method="POST" action="{{ route('user.login') }}" class="login-form">
+            @csrf
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+            <!-- Email Address -->
+            <div>
+                <x-input-label for="email" value="メールアドレス" />
+                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            </div>
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">ログイン状態を保持する</span>
-            </label>
-        </div>
+            <!-- Password -->
+            <div class="mt-4">
+                <x-input-label for="password" value="パスワード" />
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('user.password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('user.password.request') }}">
-                    パスワードをお忘れですか？
-                </a>
-            @endif
+                <x-text-input id="password" class="block mt-1 w-full"
+                                type="password"
+                                name="password"
+                                required autocomplete="current-password" />
 
-            <x-primary-button class="ml-3">
-                ログイン
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            </div>
+
+            <!-- Remember Me -->
+            <div class="block mt-4">
+                <label for="remember_me" class="inline-flex items-center">
+                    <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
+                    <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">ログイン状態を保持する</span>
+                </label>
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                @if (Route::has('user.password.request'))
+                    <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('user.password.request') }}">
+                        パスワードをお忘れですか？
+                    </a>
+                @endif
+
+                <x-primary-button class="btn btn-secondary  ml-4">
+                    ログイン
+                </x-primary-button>
+            </div>
+        </form>
+
+    </div>
+@endsection
--- resources/views/user/welcome.blade.php
+++ resources/views/user/welcome.blade.php
@@ -10,26 +10,28 @@
         <img class="main-image" src="../images/main-image.jpg" alt="店の写真">
 
         <div class="content text-center">
-            <p>新潟市西区にある美容室kamiclub(カミクラブ)</p>
-            <p>丁寧で確かな技術店内は「癒し」をテーマにしたくつろぎの異空間</p>
-            <p>完全予約、マンツーマン対応の大人のプライベートサロンです</p>
+            <p>新潟市西区にある美容室kamiclub</p>
+            <p>完全予約、少人数の落ち着いたプライベートな空間で、癒しのサロンタイム体験</p>
+            <p>M3D・G-UP縮毛矯正で圧倒的な仕上がりの差を実感。誰もがうらやむ輝く美髪へ</p>
             <p>その質と深さを、ぜひ一度ご体験ください</p>
         </div>
 
         <div class="container">
             <div class="item1">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d787.5940821925617!2d139.002868269649!3d37.85148405718964!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzfCsDUxJzA1LjMiTiAxMznCsDAwJzEyLjYiRQ!5e0!3m2!1sja!2sjp!4v1686583933041!5m2!1sja!2sjp" width="450" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3150.4206472581095!2d139.0020517763971!3d37.850446857796534!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5ff4c73f9e39f9b3%3A0x3e8bf03bdf48b392!2z576O5a655a6ka2FtaWNsdWI!5e0!3m2!1sja!2sjp!4v1686666568386!5m2!1sja!2sjp" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
 
             <div class="item2 text-center">
                 <p>~ ACCESS ~</p>
-                <p>〒123-4567 新潟市西区○○○○1-2-3
-                <br>Tel : 123-456-789
-                <br>営業時間 : 9:00～18:00 土日祝 9:00～18:00
-                <br>定休日 : 月曜日 第3日曜日
+                <p>〒950-1115 新潟県新潟市西区鳥原2161-2
+                <br>Tel : 025-333-8325
+                <br>営業時間 : 9:00～18:00 土日祝 9:00～17:00
+                <br>定休日 : 月曜日・第１火曜日・ 第3日曜日・月1日不定休
                 <br>完全予約制
                 </p>
-                <a href="{{ route('user.register') }}" class="btn btn-success">無料で登録して予約する</a>
+                <a href="{{ route('user.register') }}" class="btn btn-secondary">無料で登録して予約する</a>
+                <br>
+                <a href="https://beauty.hotpepper.jp/slnH000061196/" class="btn btn-secondary">ホットペッパーから予約</a>
             </div>
         </div>
 
