20599bc1812d176915cbf2f7bf65c5d6b02bec3c
osugiyuya <mande_e@ezweb.ne.jp>
2023-06-17 20:38:43 +0900

    admin_deleteRegister

--- resources/views/user/auth/login.blade.php
+++ resources/views/user/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.layout')
 
 @section('title')
-    kamiclub
+    kamiclub | ログイン
 @endsection
 
 @section('content')
--- resources/views/user/auth/register.blade.php
+++ resources/views/user/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.layout')
 
 @section('title')
-    kamiclub
+    kamiclub | 新規登録
 @endsection
 
 @section('content')
