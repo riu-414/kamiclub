4ccc9b0706ecbe0d7c839a9289ab31c48e6631e7
osugiyuya <mande_e@ezweb.ne.jp>
2023-07-03 01:39:23 +0900

    adminReserve_past

--- resources/views/admin/reserve/past.blade.php
+++ resources/views/admin/reserve/past.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl leading-tight">
-            管理者用 : 今日以降の予約
+            管理者用 : 過去の予約確認
         </h2>
     </x-slot>
 
@@ -16,9 +16,7 @@
                             <div class="w-full mx-auto overflow-auto">
 
                                 <div class="flex justify-end mb-4">
-                                    <button onclick="location.href='{{ route('admin.reserve.create') }}'" class="mr-8 bg-blue-300 border-0 py-2 px-8 focus:outline-none hover:bg-blue-400 rounded text-lg">新規登録</button>
-
-                                    <button onclick="location.href='{{ route('admin.reserve.past') }}'" class="bg-blue-300 border-0 py-2 px-8 focus:outline-none hover:bg-blue-400 rounded text-lg">過去の予約</button>
+                                    <button onclick="location.href='{{ route('admin.reserve.index') }}'" class="bg-blue-300 border-0 py-2 px-8 focus:outline-none hover:bg-blue-400 rounded text-lg">戻る</button>
                                 </div>
 
                                 <table class="table-auto w-full text-left whitespace-no-wrap">
@@ -55,18 +53,6 @@
 
                 </div>
             </div>
-
-            <div class="main mt-6 overflow-hidden sm:rounded-lg">
-                <div class="p-6">
-
-                    今日の予定
-
-                    {{-- <livewire:calendar /> --}}
-                    {{-- @livewire('calendar') --}}
-
-                </div>
-            </div>
-
 
         </div>
     </div>
